d8f6be1290b4e236f74ecff3be960106b480d721
“Douglas” <douglas.e.mckinley@gmail.com>
2025-04-07 08:50:32 -0700

    source venv

--- hello-world.py
+++ hello-world.py
@@ -3,3 +3,5 @@ print('hello world')
 # cool, I'm running python from /usr/bin/python
 
 # and now I'm running from venv : /Users/douglasmckinley/VSCodeProjects/qad-doc-folder/qad-doc/.venv/bin/python
+
+# source .venv/bin/activate
