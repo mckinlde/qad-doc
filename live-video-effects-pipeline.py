50e156a96b79113496528d897a590d1bb9bc2f59
“Douglas” <douglas.e.mckinley@gmail.com>
2025-04-07 12:05:18 -0700

    I don't wanna O(n^2) these streams dawg

--- live-video-effects-pipeline.py
+++ live-video-effects-pipeline.py
@@ -14,27 +14,22 @@ output_video_stream.width = input_video_stream.width
 output_video_stream.height = input_video_stream.height
 output_video_stream.pix_fmt = 'yuv420p'
 
-# Set up audio stream (copy directly)
+# Set up audio stream (pass through packets)
 input_audio_stream = next((s for s in input_container.streams if s.type == 'audio'), None)
 if input_audio_stream:
-    output_audio_stream = output_container.add_stream('copy', template=input_audio_stream)
+    output_audio_stream = output_container.add_stream(input_audio_stream.codec.name)
 else:
     output_audio_stream = None
 
 print("init'd streams")
 
-# Keep track of audio packets separately
 for packet in input_container.demux():
-    if packet.stream.type == 'video':
+    if packet.stream == input_video_stream:
         for frame in packet.decode():
-            # a little too much printing here print('video frame')
             img = frame.to_ndarray(format='bgr24')
-
-            # Apply OpenCV effects
             processed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             processed_img = cv2.cvtColor(processed_img, cv2.COLOR_GRAY2BGR)
 
-            # Create new video frame
             new_frame = av.VideoFrame.from_ndarray(processed_img, format='bgr24')
             new_frame.pts = frame.pts
             new_frame.time_base = frame.time_base
@@ -42,10 +37,9 @@ for packet in input_container.demux():
             for out_packet in output_video_stream.encode(new_frame):
                 output_container.mux(out_packet)
 
-    elif packet.stream.type == 'audio' and output_audio_stream:
-        # Copy audio packets directly
-        packet.stream = output_audio_stream  # redirect to output
-        output_container.mux(packet)
+    elif output_audio_stream and packet.stream == input_audio_stream:
+        if packet.dts is not None:
+            output_container.mux(packet)
 
 # Flush video encoder
 for packet in output_video_stream.encode():
