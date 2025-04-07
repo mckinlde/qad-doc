8c659c7e1802619cfbf430b11994bb762e93f6df
“Douglas” <douglas.e.mckinley@gmail.com>
2025-04-07 13:37:25 -0700

    human comments labeling what's what

--- live-video-effects-pipeline.py
+++ live-video-effects-pipeline.py
@@ -23,9 +23,13 @@ else:
 
 print("init'd streams")
 
-for packet in input_container.demux():
+for packet in input_container.demux(): # MUX
     if packet.stream == input_video_stream:
-        for frame in packet.decode():
+        for frame in packet.decode(): # DECODE
+
+            # Let's do some frame processing - black and white because 
+            # it may feel like the 80's were 20 years ago but they 
+            # were actually 40 years ago
             img = frame.to_ndarray(format='bgr24')
             processed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             processed_img = cv2.cvtColor(processed_img, cv2.COLOR_GRAY2BGR)
@@ -34,16 +38,17 @@ for packet in input_container.demux():
             new_frame.pts = frame.pts
             new_frame.time_base = frame.time_base
 
-            for out_packet in output_video_stream.encode(new_frame):
-                output_container.mux(out_packet)
+            for out_packet in output_video_stream.encode(new_frame): # ENCODE
+                output_container.mux(out_packet) # MUX Video
 
+    # Passthrough audio
     elif output_audio_stream and packet.stream == input_audio_stream:
         if packet.dts is not None:
-            output_container.mux(packet)
+            output_container.mux(packet) # MUX Audio
 
 # Flush video encoder
 for packet in output_video_stream.encode():
-    output_container.mux(packet)
+    output_container.mux(packet) # MUX Video+Audio
 
 print('end flush')
 input_container.close()
